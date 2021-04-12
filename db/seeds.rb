d0dce259979d7d97bc4ef463cfc8e8fc00d7ef52
James Yeates <jimibue@gmail.com>
2021-04-12 11:09:06 -0600

    features

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,36 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-
-Skill.create(name: 'ruby', description:'Syntaz Sugar')
-Skill.create(name: 'rails', description:'Stay on the rails...Convention')
-Skill.create(name: 'react', description:'make writing UI noooice!!!')
-
-# Skills could also be a homework with a grade
-Skill.create(name: 'Homework1', description:'CRUD ACTIONS!!')
-Skill.create(name: 'Homework2', description:'Cassino')
-
-5.times do
-  user = User.create(name: Faker::Name.name)
-
-  # each user will have a grade for each skill
-  5.times do |i|
-   Grade.create(user_id: user.id, skill_id: i, score: rand(100))
- end
-end
-
-Skill.create(name: 'Java', description:'OOP')
-Skill.create(name: 'Vue', description:'Reacts competition')
-
-User.create(name: Faker::Name.name)
-User.create(name: Faker::Name.name)
-
-puts "SKILLZZ SIZE: #{Skill.all.length}"
-puts "USER SIZE: #{User.all.length}"
-puts "GRADE SIZE: #{Grade.all.length}"
-
-# grab users skill
-puts "FIRST USER SKILLs: #{User.first.skills}"
-
-# grab users firts skill grades
-puts "FIRST USER Grades: #{User.first.grades}"
+Meme.create(text:Faker::ChuckNorris.fact, img_url:'https://res-console.cloudinary.com/dcc4i2z2u/thumbnails/transform/v1/image/upload//v1602029583/YzIuanBlZw==/drilldown?0.20299489738897503' )
+Meme.create(text:Faker::ChuckNorris.fact, img_url:'https://res-console.cloudinary.com/dcc4i2z2u/thumbnails/transform/v1/image/upload//v1602029583/YzIuanBlZw==/drilldown?0.20299489738897503' )
